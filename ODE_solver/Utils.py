03ca482c2498ebbbd4cacd809af84512d13ca528
Hadar Sharvit <hadar.sharvit1@mail.huji.ac.il>
2022-09-16 18:24:34 +0300

    fixed the energy-landscape plot

--- ODE_solver/Utils.py
+++ ODE_solver/Utils.py
@@ -1,52 +1,14 @@
 import numpy as np
 from simulation import RobotSimulation
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 
-# TODO: add grid plot of sum of rewards to evaluate the model w.r.t Acos(wt) - "Energy landscape"
-# TODO: check how to access the loss to add regularization - ask Aviv maybe
-# TODO: add minimal work to the reward - see image on iPhone
-# TODO: is it ok that the state is only the LAST phi?
-# TODO: check that the lift force makes sense!
-
-def energy_landscape():
-    # might want to vectorize later
-    N = 20
-    end_time = 3  # seconds
-    max_amplitude = 0.02  # Nm
-    max_freq = 40  # Hz
-    A = np.linspace(0.001, max_amplitude, N)  # (N,1)
-    F = np.linspace(0.001, max_freq, N)  # (N,1)
-    all_rewards = []
-    for a in A:
-        for f in F:
-            print(f"A=[{a:.4f}/{max_amplitude}],"
-                  f" f=[{f:.4f}/{max_freq}]",
-                  end='\r')
-            t = np.linspace(0, end_time, N)  # (N,1)
-            torque = a * np.cos(2 * np.pi * f * t)  # (N,1)
-            start_t, end_t, delta_t = 0, 0.05, 0.001
-            sim = RobotSimulation()
-            cumulative_lift_reward = 0
-            for action in torque:
-                sim.set_motor_torque(lambda x: action)
-                sim.solve_dynamics()
-                phi, phi_dot = sim.solution
-                phi0, phidot0 = phi[-1], phi_dot[-1]
-                start_t = end_t
-                end_t += 0.05
-                sim.set_init_cond(phi0, phidot0)
-                sim.set_time(start_t, end_t)
-                cumulative_lift_reward += (sim.drag_torque(phi_dot)).mean()
-            all_rewards.append(cumulative_lift_reward)
-    grid = np.array(all_rewards).reshape((N, N))
-    h = plt.contourf(F, A, grid)
-    # plt.axis('scaled')
-    plt.title(r"Accumulated Lift reward $\langle F_{lift} \rangle =\frac{1}{2}\rho_{air} A_{wing} C_{drag}\dot\phi^2$")
-    plt.xlabel("Frequency [Hz]")
-    plt.ylabel("Amplitude [m]")
-    plt.colorbar()
-    plt.show()
+# TODO: add grid plot of sum of rewards to evaluate the model w.r.t Acos(wt) - "Energy landscape"  (V)
+# TODO: check how to access the loss to add regularization - ask Aviv maybe                        (X)
+# TODO: add minimal work to the reward - see image on iPhone                                       (X)
+# TODO: is it ok that the state is only the LAST phi?                                              (X)
+# TODO: check that the lift force makes sense                                                      (V)
 
 
 def plot_all(time, phi, phi_dot, phi_ddot, alpha, force, torque, phi0, phidot0):
@@ -119,34 +81,38 @@ def check_simulation_given_torque(torque: np.ndarray, torque_name: str, do_plot:
     return phi_arr, phi_dot_arr, phi_ddot_arr, time_arr, ang_arr, force_arr
 
 
-def energy_landscape2():
+def energy_landscape():
     # might want to vectorize later
-    N = 3
-    end_time = 3  # seconds
-    max_amplitude = 0.02  # Nm
+    N = 30
+    end_time = 60  # seconds
+    max_amplitude = 0.025  # Nm
     max_freq = 40  # Hz
-    A = np.linspace(0.001, max_amplitude, N)  # (N,1)
-    F = np.linspace(0.001, max_freq, N)  # (N,1)
+    amplitude_arr = np.linspace(0, max_amplitude, N)  # (N,1)
+    freq_array = np.linspace(0, max_freq, N)  # (N,1)
     all_rewards = []
-    for a in A:
-        for f in F:
-            print(f"A=[{a:.4f}/{max_amplitude}],"
+    for i, a in enumerate(amplitude_arr):
+        for j, f in enumerate(freq_array):
+            print(f"[{j + i * N + 1}/{N * N}] "
+                  f"A=[{a:.4f}/{max_amplitude}],"
                   f" f=[{f:.4f}/{max_freq}]",
                   end='\r')
             t = np.linspace(0, end_time, N)  # (N,1)
             torque = a * np.cos(2 * np.pi * f * t)  # (N,1)
             torque_name = f"{a:.3f}cos(2" + r"$\pi$" + f"{f:.3f}t)"
-            _, _, _, _, _, force_arr = check_simulation_given_torque(torque, torque_name, True)
+            _, _, _, _, _, force_arr = check_simulation_given_torque(torque, torque_name, False)
             all_rewards.append(np.mean(force_arr))
     grid = np.array(all_rewards).reshape((N, N))
-    h = plt.contourf(F, A, grid)
+    h = plt.contourf(freq_array, amplitude_arr, grid)
     # plt.axis('scaled')
     plt.title(r"Accumulated Lift reward $\langle F_{lift} \rangle =\frac{1}{2}\rho_{air} A_{wing} C_{drag}\dot\phi^2$")
     plt.xlabel("Frequency [Hz]")
-    plt.ylabel("Amplitude [m]")
+    plt.ylabel("Amplitude [Nm]")
     plt.colorbar()
     plt.show()
 
 
 if __name__ == '__main__':
-    energy_landscape2()
+    # torque = 0.02 * (np.cos(2 * np.pi * np.linspace(0, 2, 60)))
+    # torque_name = r"$0.02cos(2\pi t)$"
+    # check_simulation_given_torque(torque, torque_name, True)
+    energy_landscape()
