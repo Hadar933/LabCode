still didnt fix the force problem. theres is a problem with scheduling the angle shift and the sign of phi_dot, so for now I calcualte the angle given phi_dot, which is ok since it doesn't affect the ODE (its always cos(2*angle)=1

ODE_solver/simulation.py
@@ -113,11 +113,12 @@ class RobotSimulation:
     def lift_force(self, phi_dot: np.ndarray, angle: np.ndarray) -> np.ndarray:
         """
         calculated the drag force on the wing, which will be used as reward
-        TODO: theoretically phi_dot here will have all + or all - sign, as we separate zero crosses, no? YES
+        TODO: theoretically phi_dot here will have all + or all - sign, as we separate zero crosses, no?
         :param angle:
         :param phi_dot:
         :return:
         """
+        angle = np.where(phi_dot > 0, RADIAN45, RADIAN135)
         c_lift = self.c_lift(angle)
         f_lift = 0.5 * AIR_DENSITY * WING_AREA * c_lift * phi_dot * np.abs(phi_dot)
         return f_lift

ODE_solver/utils.py
@@ -64,7 +64,7 @@ def check_simulation_given_torque(torque: np.ndarray, torque_name: str, do_plot:
     """
     phi_arr, phi_dot_arr, phi_ddot_arr, ang_arr, time_arr, force_arr, torque_arr = [], [], [], [], [], [], []
     start_t, end_t = 0, 0.2  # TODO: playing with the timing here - I think that setting += delta_t causes bugs. we dont need to sync the action timing with the cosine timing
-    sim = RobotSimulation(0, 2e-4, start_t, end_t)
+    sim = RobotSimulation(0, 0, start_t, end_t)
 
     phi0_name = sim.phi0
     phi_dot0_name = sim.phi_dot0