5f37bd8e5ed98a81f8f2d734c98390dba01fedb4
Hadar Sharvit <hadar.sharvit1@mail.huji.ac.il>
2022-10-14 13:21:50 +0300

    minor aesthetic changes

--- ODE_solver/agent.py
+++ ODE_solver/agent.py
@@ -24,70 +24,71 @@ class TensorboardCallback(BaseCallback):
         return True
 
 
-def automated_env_checker(env):
-    print("Testing Environment")
-    check_env(env)
-    print("Finished Testing Environment")
-
-
-def train_model(env, timesteps, steps_str, use_tensorboarad_in_colab):
+def train_model_and_save(env: WingEnv, steps_to_train: int, name: str, use_tensorboard_in_colab: bool) -> None:
     print("Training Model")
     env.reset()
-    if use_tensorboarad_in_colab:
+    if use_tensorboard_in_colab:
         model = PPO("MultiInputPolicy", env, device='cuda', tensorboard_log='/content/tensorboard')
-        model.learn(total_timesteps=timesteps, callback=TensorboardCallback(env))
+        model.learn(total_timesteps=steps_to_train, callback=TensorboardCallback(env))
     else:
         model = PPO("MultiInputPolicy", env)
-        model.learn(total_timesteps=timesteps)
-    model.save(f"PPO_{steps_str}")
+        model.learn(total_timesteps=steps_to_train)
+    model.save(name)
     del model
-    print("Finished Training Model")
 
 
-def simulate_steps(env, steps_str, n_steps):
+def load_model_and_invoke(env: WingEnv, name: str, n_steps: int):
     obs = env.reset()
-    model = PPO.load(f"PPO_{steps_str}.zip")
-    R, S, A, time = [0.0], [0.0], [0.0], [0.0]
-
+    model = PPO.load(f"{name}.zip")
+    rewards, states, actions, time = [0.0], [0.0], [0.0], [0.0]
     for i in range(n_steps - 1):
         action, _states = model.predict(obs)
         obs, reward, done, info = env.step(action)
-        R.append(reward)
-        S.append(obs['phi'][-1])
-        A.append(action.item())
+        rewards.append(reward)
+        states.append(obs['phi'][-1])
+        actions.append(action.item())
         time.append((i + 1) * env.step_time)
-        if done:
-            obs = env.reset()
+        # if done: obs = env.reset()
     env.close()
-    return R, S, A, time
+    return rewards, states, actions, time
 
 
-def plot_steps(R, S, A, time, steps_str, save):
+def plot_steps(rewards, states, actions, time, name, save):
     fig, axs = plt.subplots(3, 1)
     fig.set_size_inches(18, 8)
-    fig.suptitle(f"PP0 {steps_str} steps (SB3)")
-    axs[0].plot(time, R, linewidth=0.8)
+    fig.suptitle(name)
+
+    axs[0].plot(time, rewards, linewidth=0.8)
     axs[0].set(ylabel=r"Reward [Arb.U]")
-    axs[1].plot(time, A, linewidth=0.8)
+
+    axs[1].plot(time, actions, linewidth=0.8)
     axs[1].set(ylabel=r"Action $\tau [Nm]$")
-    axs[2].plot(time, S, linewidth=0.8)
+
+    axs[2].plot(time, states, linewidth=0.8)
     axs[2].set(ylabel=r"State $\phi [rad]$")
     axs[2].set(xlabel='time [sec]')
     axs[2].axhline(y=np.pi, color='r', linestyle='--')
     axs[2].text(x=900, y=np.pi + 0.3, s=r"$\phi=\pi$")
     axs[2].axhline(y=0, color='r', linestyle='--')
     axs[2].text(x=900, y=0.3, s=r"$\phi=0$")
-    if save: plt.savefig(f"PP0 {steps_str} steps (SB3)")
+
+    if save: plt.savefig(f"{name}_fig")
     plt.show()
 
 
 if __name__ == '__main__':
     in_colab = 'COLAB_GPU' in os.environ
     print(f"Working in colab: {in_colab}")
-    n_steps = 180_000
-    steps_str = f"{str(n_steps)[:-3]}k_regularized_action"
-    env = WingEnv()
-    automated_env_checker(env)
-    train_model(env, n_steps, steps_str, in_colab)
-    R, S, A, time = simulate_steps(env, steps_str, 1000)
-    plot_steps(R, S, A, time, steps_str, in_colab)
+    n_train_steps = 1_000
+    invoke_for = 1000
+    model_name = f"PPO_{str(n_train_steps)[:-3]}k"
+    plot_after_invocation = True
+
+    wing_env = WingEnv()
+    check_env(wing_env)
+
+    if f"{model_name}.zip" not in os.listdir():  # need to train
+        train_model_and_save(wing_env, n_train_steps, model_name, in_colab)
+
+    R, S, A, T = load_model_and_invoke(wing_env, model_name, invoke_for)
+    if plot_after_invocation: plot_steps(R, S, A, T, model_name, in_colab)
